4d25601e51234c5ca176841912326a24afce2a51
byLugos <bylugos147@gmail.com>
2024-11-28 18:51:50 -0500

    Initial config

---
+++ src/main/java/com/parcial/electiva/ElectivaApplication.java
@@ -0,0 +1,11 @@
+package com.parcial.electiva;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class ElectivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectivaApplication.class, args);
+	}
+
+}
